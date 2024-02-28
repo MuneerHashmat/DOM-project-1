8435725380c40c08f564ece9aa78b9fe8a042bf6
Muneer Hashmat <muneerhashmat@gmail.com>
2024-02-28 13:16:41 +0530

    work done

---
+++ script.js
@@ -0,0 +1,13 @@
+let div = document.getElementById("container");
+
+div.style.backgroundColor = "yellow";
+div.style.margin = "20px";
+div.style.padding = "10px";
+div.style.fontSize = "18px";
+div.style.fontWeight = "bold";
+div.style.height = "200px";
+div.style.width = "300px";
+
+//extra property not mentioned in rubrics
+div.style.color = "blue";
+
